d97aa421e2e04652d2953d195b956829094aadb4
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-06-26 20:54:54 -0300

    Criando diferentes métodos para testar as Exceptions

--- Exceptions/ConsoleUI/Program.cs
+++ Exceptions/ConsoleUI/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ExceptionsLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace ConsoleUI
 {
@@ -10,6 +12,19 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            DemoCode demo = new DemoCode();
+            try
+            {
+                int result = demo.GrandParentMethod(10);
+                Console.WriteLine($"The value at the given position is {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Exceptions/ExceptionsLibrary/DemoCode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsLibrary
+{
+    public class DemoCode
+    {
+        public int GrandParentMethod(int position)
+        {
+            int output = 0;
+            Console.WriteLine("Open DataBase Connection");
+
+            try
+            {
+                output = ParentMethod(position);
+            }
+            catch (Exception ex)
+            {
+                //Do some logging
+                throw new Exception("Fiz merda", ex);
+            }
+            finally
+            {
+                Console.WriteLine("Close DataBase Connection");
+            } 
+            
+            return output;
+        }
+        
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+
+        public int GetNumber(int position)
+        {
+            int output = 0;
+
+            int[] numbers = new int[] { 1, 4, 7, 2 };
+            output = numbers[position];
+
+            return output;
+        }
+    }
+}
