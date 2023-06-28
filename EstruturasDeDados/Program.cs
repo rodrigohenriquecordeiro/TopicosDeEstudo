d74f3350cde70f53ff9f273544587cd10a4b7630
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-06-28 19:59:44 -0300

    Iniciado estudo com Estruturas de Dados

---
+++ EstruturasDeDados/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturasDeDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Series> lstSeries = new List<Series>
+            {
+                new Series { NomeDaSerie = "The Diplomat", Streaming = "Netflix" },
+                new Series { NomeDaSerie = "Secreat Invasion", Streaming = "Disney+" },
+                new Series { NomeDaSerie = "American Dad!", Streaming = "Star+" },
+                new Series { NomeDaSerie = "Girls", Streaming = "HBO Max" },
+                new Series { NomeDaSerie = "Good Omens", Streaming = "Prime Video" },
+            };
+
+            foreach (var item in lstSeries)
+            {
+                Console.WriteLine($"Série: {item.NomeDaSerie} - Streaming: {item.Streaming}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EstruturasDeDados/Series.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturasDeDados
+{
+    public class Series
+    {
+        public string NomeDaSerie { get; set; }
+        public string Streaming { get; set; }
+    }
+}
