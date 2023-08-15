bcae4b1c1151fecaf51d404e36feeb7d88a6b67c
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-08-15 19:21:58 -0300

    Criado caderno para testes

---
+++ Caderno/Program.cs
@@ -0,0 +1,8 @@
+﻿double altura = 1.70;
+double peso = 75;
+string corDosOlhos = "Azul";
+
+if (altura > 1.60 || peso == 60 && corDosOlhos != "Verde")
+    Console.WriteLine("Sim");
+else
+    Console.WriteLine("Não");
