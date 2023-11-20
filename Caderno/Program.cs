caddd9fc429076e0d8f6632faccb6e1d5ed9165c
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2023-11-20 15:32:51 -0300

    Ajustes

--- Caderno/Program.cs
+++ Caderno/Program.cs
@@ -1,4 +1,34 @@
-﻿for (int i = 1; i < 1000000; i++)
+﻿internal class Program
 {
-    Console.WriteLine(i);
+    private static void Main(string[] args)
+    {
+        Dudu();
+
+        //Rodrigo();
+
+        Console.ReadLine();
+    }
+
+    static void Dudu()
+    {
+        string nome = "";
+
+        Console.Write("Digite um nome: ");
+        nome = Console.ReadLine();
+
+        if (nome == "Dudu")
+        {
+            Console.WriteLine("Acesso liberado!");
+        }
+        else
+        {
+            Console.WriteLine("Acesso negado");
+        }
+    }
+
+    static void Rodrigo()
+    {
+        Console.Write("Digite um nome: "); 
+        Console.Write(string.Equals(Console.ReadLine(), "Rodrigo") ? "Acesso liberado" : "Acesso negado!");
+    }
 }
